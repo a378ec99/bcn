d52fb02233c8bb2cbdfd4373bc230a1f6bce1cb1
a378ec99 <sebastian.ohse@mailbox.org>
2017-12-14 10:32:20 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,7 @@ setup(
     url='https://github.com/a378ec99/bcn',
     license='LICENSE.txt',
     long_description=open('README.txt').read(),
-    keywords=('optimization,manifold optimization,automatic differentiation,'
-              'machine learning,numpy,scipy,theano,autograd,tensorflow'),
+    keywords=('bias recovery, blind, manifold optimization'),
     packages=find_packages(exclude=['tests']),
     install_requires=['anaconda >= 5.0.1', 'pymanopt >= 0.2.3', 'mpi4py >= 3.0.0'],
 )
