53f14d0455b2594b0dda15fd4406930a2b5d60bf
a378ec99 <sebastian.ohse@mailbox.org>
2017-12-20 13:44:16 +0100

    minor fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='BCN',
+    name='bcn',
     version='0.1.0',
     description='Blind Compressive Normalization',
     author='Sebastian Ohse',
@@ -11,6 +11,6 @@ setup(
     long_description=open('README.me').read(),
     keywords=('bias', 'normalization', 'blind', 'manifold optimization', 'compressed sensing', 'high-dimensional'),
     packages=find_packages(exclude=['tests']),
-    install_requires=['scipy >= 0.19.0', 'numpy >= 1.11.3', 'sklearn >= 0.18.1', 'pymanopt >= 0.2.3', 'mpi4py >= 3.0.0', 'matplotlib >= 1.5.3', 'seaborn >= 0.7.1', 'mpi4py >= 3.0.0']
+    install_requires=['scipy >= 0.19.0', 'numpy >= 1.11.3', 'sklearn >= 0.18.1', 'pymanopt >= 0.2.3', 'matplotlib >= 1.5.3', 'seaborn >= 0.7.1', 'mpi4py >= 3.0.0']
 )
 
