ffe748aab719ddd532de0bec7590cae4d2d56eef
Sebastian Ohse <sebastian.ohse@mol-med.uni-freiburg.de>
2018-05-09 13:19:45 +0000

    completed example and sparse

--- bcn/cost.py
+++ bcn/cost.py
@@ -12,7 +12,7 @@ import autograd.numpy as ag
 
 class Cost(object):
 
-    def __init__(self, A, y):
+    def __init__(self, A, y, sparsity):
         """Creates a cost function based on autograd with a linear operator A and target y.
         
         Parameters
@@ -22,16 +22,19 @@ class Cost(object):
         y : list; elements=float, len=n_measuremnts
             Target vector.
         """
-        self.A = A
-        self.y = np.array(y)
-
+        self.sparsity = sparsity
+        self.A_rows = ag.array([a['row'] for a in A])
+        self.A_cols = ag.array([a['col'] for a in A])
+        self.A_values = ag.array([a['value'] for a in A])
+        self.y = ag.array(y)
+        
     def cost_func(self, X):
-        """Cost function for evaluationg linear operator A and measurements y at X.
+        """Cost function for evaluationg linear operator A and targets y at X.
 
         Parameters
         ----------
         X : numpy.ndarray; shape=(n_samples, n_features)
-            Measured data matrix containing mixed signal and noise.
+            Estimated bias matrix.
 
         Returns
         -------
@@ -39,26 +42,15 @@ class Cost(object):
             Squared mean error.
 
         Note
-        ----
+        ----        
         Size scaling with ag.mean is not nessesary for convergence.
         """
-        
-        if isinstance(X, tuple):
+        if len(X) == 3: # WARNING type changes from list to autograd.builtins.SequenceBox after first run.
             usvt = X
             X = ag.dot(usvt[0], ag.dot(ag.diag(usvt[1]), usvt[2]))
-
-        if np.isnan(X).any():
-            print 'Warning: X contains nan values. Converting those to zero.'
-            X = np.nan_to_num(X, copy=False)
-            
-        y_est = []
-        for measurement in self.A:
-            sum_ = 0.0
-            for i, j, value in zip(measurement['row'], measurement['col'], measurement['value']):
-                sum_ = sum_ + value * X[i, j]
-            y_est.append(sum_)
-            
-        y_est = np.array(y_est)
+        else:
+            assert np.isfinite(X).all()
+        y_est = ag.sum(self.A_values * X[self.A_rows, self.A_cols].reshape(-1, self.sparsity), axis=1)
         error = ag.mean((y_est - self.y)**2)
         return error
 
--- tests/test_cost.py
+++ tests/test_cost.py
@@ -17,53 +17,56 @@ from bcn.cost import Cost
 class TestSimple(unittest.TestCase):
 
     def setUp(self):
+        self.sparsity = 1
         self.X = np.ones((10, 9))
         self.X[0, :] = 2.0
         self.y = [0.0, 4.0]
         self.A = [{'row': [0], 'col': [8], 'value': [3.5]},
                   {'row': [9], 'col': [7], 'value': [4.5]}]
-            
+       
     def test_entry(self):
-        cost = Cost(self.A, self.y)
+        cost = Cost(self.A, self.y, self.sparsity)
         expected = np.mean([(0.0 - self.X[0, 8] * 3.5)**2, (4.0 - self.X[9, 7] * 4.5)**2])
         assert cost.cost_func(self.X) == expected
        
     def test_ksparse(self):
+        sparsity = 2 
         A = [{'row': [0, 4], 'col': [8, 4], 'value': [3.5, 1.5]},
              {'row': [9, 5], 'col': [7, 8], 'value': [4.5, 0.9]}]
-        cost = Cost(A, self.y)
+        cost = Cost(A, self.y, sparsity)
         expected = np.mean([(0.0 - (self.X[0, 8] * 3.5 + self.X[4, 4] * 1.5))**2, (4.0 - (self.X[9, 7] * 4.5 + self.X[5, 8] * 0.9))**2])
         assert cost.cost_func(self.X) == expected
-    
+
     def test_dense(self):
+        sparsity = self.X.shape[0] * self.X.shape[1]
         row, col = np.indices(self.X.shape)
         A = [{'row': list(row.ravel()), 'col': list(col.ravel()), 'value': range(90)}]
         y = list(np.ones(90) * 1.4)
-        cost = Cost(A, y)
+        cost = Cost(A, y, sparsity)
         A_matrix = np.reshape(range(90), (10, 9))
         expected = np.mean((y - np.sum([A_matrix * self.X]))**2)
         assert cost.cost_func(self.X) == expected
-       
+   
     def test_X_nan(self):
         X = np.array(self.X)
         X[0, :] = np.nan
-        cost = Cost(self.A, self.y)
-        expected = np.mean([(0.0 - 0.0 * 3.5)**2, (4.0 - X[9, 7] * 4.5)**2])
-        assert cost.cost_func(X) == expected
-        
+        cost = Cost(self.A, self.y, self.sparsity)
+        with self.assertRaises(AssertionError):
+            cost.cost_func(X)
+            
     def test_zero_A(self):
         A_zero = [{'row': [0], 'col': [8], 'value': [0.0]},
                   {'row': [9], 'col': [7], 'value': [0.0]}]
-        cost = Cost(A_zero, self.y)
+        cost = Cost(A_zero, self.y, self.sparsity)
         expected = np.mean([0.0, (4.0 - 0.0)**2])
         assert cost.cost_func(self.X) == expected
         
     def test_zero_X(self):
         X_zero = np.zeros((10, 9))
-        cost = Cost(self.A, self.y)
+        cost = Cost(self.A, self.y, self.sparsity)
         expected = np.mean([0.0, (4.0 - 0.0)**2])
         assert cost.cost_func(X_zero) == expected
-        
+    
 '''    
 class TestUseCase(unittest.TestCase):
     """Test to verify that the cost function produces the correct result..
